32f541bbfe0882ddd626d07cf85bad825cd3a41f
zhengjinyang <y1519333039@163.com>
2019-06-27 08:50:48 +0800

    js

--- frontEnd/pages/index/index_module.js
+++ frontEnd/pages/index/index_module.js
@@ -1,31 +1,3 @@
-// import Base from '../../utils/base.js'
-// let base=new Base();
-// class Alls extends Base{
-//   constructor(){
-//     super()
-//   }
-//   goods(backData){
-//     let respon={
-//       url:'goods',
-//       type:'get',
-//       getData:function (data){
-//         backData && backData(data)
-//       }
-//     }
-//     base.baseUrl(respon)
-//   }
-//   banner(backData){
-//     let respon = {
-//       url: 'index.php?s=api/v1/index',
-//       type: 'get',
-//       getData: function (data) {
-//         backData && backData(data)
-//       }
-//     }
-//     base.baseUrl(respon)
-//   }
-// }
-// export default Alls
 
 import Base from '../../utils/base.js'
 let base=new Base();
